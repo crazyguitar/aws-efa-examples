b57524622cf544a35562efdc30fe214d02e59627
Chang-Ning Tsai <changnit@amazon.com>
2025-09-28 18:12:07 -0700

    add func doc in taskset
    Signed-off-by: Chang-Ning Tsai <changnit@amazon.com>

--- src/affinity/include/common/taskset.h
+++ src/affinity/include/common/taskset.h
@@ -18,6 +18,11 @@
   } while (0)
 
 struct Taskset {
+  /**
+   * @brief Bind current process to a specific CPU core
+   * @param cpu CPU core ID to bind to
+   * @throws std::runtime_error if sched_setaffinity fails
+   */
   inline static void Set(int cpu) {
     cpu_set_t mask;
     CPU_ZERO(&mask);
@@ -27,6 +32,11 @@ struct Taskset {
     TASKSET_CHECK(sched_setaffinity(pid, sizeof(mask), &mask));
   }
 
+  /**
+   * @brief Bind current process to multiple CPU cores
+   * @param cpus Vector of CPU core IDs to bind to
+   * @throws std::runtime_error if sched_setaffinity fails
+   */
   inline static void Set(std::vector<int> cpus) {
     cpu_set_t mask;
     CPU_ZERO(&mask);
